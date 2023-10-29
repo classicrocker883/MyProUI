40544d180d657984d1a477d5c267edb73d7b76e3
classicrocker883 <andrewleduc88@yahoo.com>
2023-10-29 03:33:43 -0400

    fix for Usart -> usart

--- Marlin/src/HAL/HC32F46x/MarlinSerial.h
+++ Marlin/src/HAL/HC32F46x/MarlinSerial.h
@@ -20,7 +20,7 @@
  *
  */
 #pragma once
-#include <drivers/usart/Usart.h>
+#include <drivers/usart/usart.h>
 #include "../../core/serial_hook.h"
 
 // optionally set uart IRQ priority to reduce overflow errors
