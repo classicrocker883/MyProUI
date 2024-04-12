cc24d33053065ec8436933cc617b3db963f70e35
wlkmanist <vlad.king5555@mail.ru>
2024-04-13 02:04:31 +0300

    Update - fix BACK_ITEM for TMC submenu (#120)
    ---------
    
    Signed-off-by: wlkmanist <vlad.king5555@mail.ru>
    Co-authored-by: Andrew <18502096+classicrocker883@users.noreply.github.com>

--- Marlin/src/lcd/e3v2/proui/dwin.cpp
+++ Marlin/src/lcd/e3v2/proui/dwin.cpp
@@ -3550,7 +3550,11 @@ void Draw_Tune_Menu() {
   void Draw_TrinamicConfig_menu() {
     checkkey = Menu;
     if (SET_MENU(TrinamicConfigMenu, MSG_TMC_DRIVERS, 5)) {
-      BACK_ITEM(Draw_AdvancedSettings_Menu);
+      #if NONE(AUTO_BED_LEVELING_UBL, AUTO_BED_LEVELING_BILINEAR, MESH_BED_LEVELING)
+        BACK_ITEM(Draw_AdvancedSettings_Menu);
+      #else
+        BACK_ITEM(Draw_Advanced_Menu);
+      #endif
       #if AXIS_IS_TMC(X)
         EDIT_ITEM(ICON_TMCXSet, MSG_TMC_ACURRENT, onDrawPIntMenu, SetXTMCCurrent, &stepperX.val_mA);
       #endif
